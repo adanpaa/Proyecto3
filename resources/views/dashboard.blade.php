33097e28d993dbe383fa8de424b0c5a72226d226
Adan Palacios Avelar <avelaradan2810@gmail.com>
2020-11-22 21:20:30 -0600

    Redirigiendo de Login a Inicio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <p align="center"><a href="\inicio" >Inicio </a></p>
     </x-slot>
 
     <div class="py-12">
