bd987c7cc8b5420d889ab19aa12ffd1acfd2b6c5
Adan Palacios Avelar <avelaradan2810@gmail.com>
2020-12-02 14:24:41 -0600

    Eager Loading Customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -17,7 +17,7 @@ class CustomerController extends Controller
     public function index()
     {
         //
-        $customers = Customer::all();
+        $customers = Customer::with('branch', 'trainer')->get();
         return view('customers/customersIndex', compact('customers'));
     }
 
